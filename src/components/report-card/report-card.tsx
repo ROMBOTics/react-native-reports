0701ded643117830d7549a2ff728f6384bfc12f1
ethanyuwang <ethanyuwang@gmail.com>
2021-02-01 09:55:28 -0800

    Update report-card.tsx

--- src/components/report-card/report-card.tsx
+++ src/components/report-card/report-card.tsx
@@ -101,9 +101,9 @@ export function ReportCard(props: ReportCardProps) {
         // handle success
         const { svgString, meta } = response.data;
         const { style } = meta;
-        const { height } = style;
+        const { height: chartHeight } = style;
         setData(svgString);
-        setHeight(height);
+        setHeight(chartHeight);
         // updateReport(response.data)
       })
       .catch((error) => {
