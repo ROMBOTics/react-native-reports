85b3f24742f7b4328c8e784acab88eedd0de9249
ethanyuwang <ethanyuwang@gmail.com>
2021-02-01 23:39:38 -0800

    Update config.ts

--- src/network/config.ts
+++ src/network/config.ts
@@ -1,7 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const reportsUrls: { [key: string]: string } = {
-  NL: 'http://localhost:3000/local/api/',
-  NLL: 'https://svc.rombot.dev/reports/api',
+  NL: 'https://svc.rombot.dev/reports/api',
   L: 'https://svc.rombot.com/reports/api',
   'L-CN': 'https://svc.rombot.cn/reports/api',
 };
